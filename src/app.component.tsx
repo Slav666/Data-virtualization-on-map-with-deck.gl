b570b731d835144c128c3b1031c020aa7a9355a9
Slawomir Dyk <slawomir.dyk@astrosat.net>
2022-11-28 15:05:38 +0000

    I created base map app with deck.gl and react-map-gl

--- src/app.component.tsx
+++ src/app.component.tsx
@@ -1,24 +1,91 @@
 import React, { FC, ReactElement } from 'react';
 
-import { Button } from '@astrosat/react-utils';
+import { ArcLayer, GeoJsonLayer } from '@deck.gl/layers/typed';
+import { DeckGL } from '@deck.gl/react/typed';
+// import DeckGL, { ArcLayer, GeoJsonLayer } from 'deck.gl';
+// import { render } from 'react-dom';
+import { NavigationControl, StaticMap, _MapContext } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+// import { Button } from '@astrosat/react-utils';
 
 import Footer from '~/layout/footer.component';
 import Header from '~/layout/header.component';
 
-const App: FC = (): ReactElement => (
-  <div className="flex min-h-screen flex-col">
-    <Header />
+// source: Natural Earth http://www.naturalearthdata.com/ via geojson.xyz
+const AIR_PORTS = 'https://d2ad6b4ur7yvpq.cloudfront.net/naturalearth-3.3.0/ne_10m_airports.geojson';
 
-    <main className="grow">
-      <h2>Main Content</h2>
+const INITIAL_VIEW_STATE = {
+  latitude: 51.47,
+  longitude: 0.45,
+  zoom: 4,
+  bearing: 0,
+  pitch: 30,
+};
+const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json';
+const NAV_CONTROL_STYLE = {
+  position: 'absolute',
+  top: 10,
+  left: 10,
+};
 
-      <Button onClick={() => console.log('BUTTON CLICKED')}>
+const App: FC = (): ReactElement => {
+  const onClick = info => {
+    if (info.object) {
+      // eslint-disable-next-line
+      alert(`${info.object.properties.name} (${info.object.properties.abbrev})`);
+    }
+  };
+  const layers = [
+    new GeoJsonLayer({
+      id: 'airports',
+      data: AIR_PORTS,
+      // Styles
+      filled: true,
+      pointRadiusMinPixels: 2,
+      pointRadiusScale: 2000,
+      getPointRadius: f => 11 - f.properties.scalerank,
+      getFillColor: [200, 0, 80, 180],
+      // Interactive props
+      pickable: true,
+      autoHighlight: true,
+      onClick,
+    }),
+    new ArcLayer({
+      id: 'arcs',
+      data: AIR_PORTS,
+      dataTransform: d => d.features.filter(f => f.properties.scalerank < 4),
+      // Styles
+      getSourcePosition: f => [-0.4531566, 51.4709959], // London
+      getTargetPosition: f => f.geometry.coordinates,
+      getSourceColor: [0, 128, 200],
+      getTargetColor: [200, 0, 80],
+      getWidth: 1,
+    }),
+  ];
+
+  return (
+    <div className="flex min-h-screen flex-col">
+      <Header />
+
+      <main>
+        <h2>Main Content</h2>
+
+        {/* <Button onClick={() => console.log('BUTTON CLICKED')}>
         <span>Click Me</span>
-      </Button>
-    </main>
+      </Button> */}
+        <DeckGL
+          ContextProvider={_MapContext.Provider}
+          controller={true}
+          initialViewState={INITIAL_VIEW_STATE}
+          layers={layers}
+        >
+          <StaticMap mapStyle={MAP_STYLE} />
+          <NavigationControl style={NAV_CONTROL_STYLE} />
+        </DeckGL>
+      </main>
 
-    <Footer />
-  </div>
-);
-
+      <Footer />
+    </div>
+  );
+};
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -11,9 +11,9 @@ import './index.css';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // <React.StrictMode>
+  <App />,
+  // </React.StrictMode>,
 );
 
 if (import.meta.env.DEV) {
